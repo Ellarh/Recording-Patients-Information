baa17a206c4e1563ca05708771c1b971b8fc72e0
Emmanuella Ndukwe-Obiukwu <ellamoves01@gmail.com>
2022-06-11 20:46:31 +0100

    Update main.py
    Simplified the code by adding a break and putting the inputs into a loop

--- main.py
+++ main.py
@@ -1,59 +1,56 @@
 from doctor import patient_logo, info_logo
 
 patients_info = [
-  {
-    "Name": "Patricia Allen",
-    "Age": 25,
-    "Nationality": "America",
-    "State of Origin": "New York",
-    "Marital Status": "Single",
-    "Gender": "Female",
-    "Purpose of Visit": "Chest Xray and blood pressure checks",
-    "Next Visit date": "28th August, 2022",
-  },
-  {
-    "Name": "Nicholas Carmen",
-    "Age": 30,
-    "Nationality": "America",
-    "State of Origin": "Seattle",
-    "Marital Status": "Single",
-    "Gender": "Male",
-    "Purpose of Visit": "Blood pressure check up and weight checks",
-    "Next Visit date": "25th September, 2022",
-  },
+    {
+        "Name": "Patricia Allen",
+        "Age": 25,
+        "Nationality": "America",
+        "State of Origin": "New York",
+        "Marital Status": "Single",
+        "Gender": "Female",
+        "Purpose of Visit": "Chest Xray and blood pressure checks",
+        "Next Visit date": "28th August, 2022",
+    },
+    {
+        "Name": "Nicholas Carmen",
+        "Age": 30,
+        "Nationality": "America",
+        "State of Origin": "Seattle",
+        "Marital Status": "Single",
+        "Gender": "Male",
+        "Purpose of Visit": "Blood pressure check up and weight checks",
+        "Next Visit date": "25th September, 2022",
+    },
 ]
-
 print(patient_logo)
 print(info_logo)
 
-record_again = True
-while record_again:
-  name = input("Patient's name: ")
-  age = int(input("Patient's age: "))
-  nation = input("Nationality: ")
-  state = input("State of Origin: ")
-  marital = input("Marital status: ")
-  gender = input("Gender: ")
-  purpose = input("Purpose of visit: ")
-  next_visit = input("Next visit date: ")
-  
-  def new_patients_info(name, age, nation, state, marital, gender, purpose, next_visit):
-    new_info = {}
-    new_info["Name"] = name
-    new_info["Age"] = age
-    new_info["Nationality"] = nation
-    new_info["State of Origin"] = state
-    new_info["Marital Status"] = marital
-    new_info["Gender"] = gender
-    new_info["Purpose of Visit"] = purpose
-    new_info["Next Visit date"] = next_visit
-    patients_info.append(new_info)
-  
+def new_patients_info(name, age, nation, state, marital, gender, purpose, next_visit):
+    patients_info.append({
+        "Name": name,
+        "Age": age,
+        "Nationality": nation,
+        "State of Origin": state,
+        "Marital Status": marital,
+        "Gender": gender,
+        "Purpose of Visit": purpose,
+        "Next Visit date": next_visit,
+    })
 
-  another_patient = input("Will you need to record another patient? 'Yes' or 'No'?  ").lower()
-  if another_patient == 'no':
-    new_patients_info(name, age, nation, state, marital, gender, purpose, next_visit)
-    print(patients_info)
-    record_again = False
-  else:
-    new_patients_info(name, age, nation, state, marital, gender, purpose, next_visit)
+while True:
+    new_patients_info(
+        input("Patient's name: "),
+        int(input("Patient's age: ")),
+        input("Nationality: "),
+        input("State of Origin: "),
+        input("Marital status: "),
+        input("Gender: "),
+        input("Purpose of visit: "),
+        input("Next visit date: "),
+    )
+    if input(
+        "Will you need to record another patient? 'Yes' or 'No'?  "
+    ).lower() == 'no':
+        break
+
+print(patients_info)
